e08e0be783218115fdcf84080b22c74cf7fb42bf
Zac <Xenofar@gmail.com>
2015-02-28 17:22:16 +1300

    A dinosaur walks into a bar

---
+++ src/ProZect K.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+	printf("Hello Kate\n");
+	return 0;
+}
---
+++ src/ProZect K.h
@@ -0,0 +1,6 @@
+#ifndef PROZECT_K_H_INCLUDED
+#define PROZECT_K_H_INCLUDED
+
+
+
+#endif // PROZECT_K_H_INCLUDED
