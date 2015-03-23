ac21e49cb164adda67f057b572764ac1c75bbb51
Zac <Xenofar@gmail.com>
2015-03-23 16:39:59 +1300

    Added Question types and structures.

--- src/ProZect K.h
+++ src/ProZect K.h
@@ -1,6 +1,38 @@
 #ifndef PROZECT_K_H_INCLUDED
 #define PROZECT_K_H_INCLUDED
 
+#include <stdlib.h>
+
 #define WIN32_LEAN_AND_MEAN
+
+typedef struct {
+	char *question;
+	int nanswer;
+	int nquestions;
+	char *options[30];
+} multichoice;
+
+typedef struct {
+	char *presentence;
+	char *correctanswer;
+	char *usersanswer;
+} fillin;
+
+enum qtype {
+	QT_MULTICHOICE = 1,
+	QT_SPOKEN,
+	QT_FILLIN,
+	QT_ESSAY
+};
+
+struct _questions{
+	enum qtype type;
+	union {
+		fillin fi;
+		multichoice mc;
+	};
+};
+
+extern struct _questions questions[];
 
 #endif // PROZECT_K_H_INCLUDED
